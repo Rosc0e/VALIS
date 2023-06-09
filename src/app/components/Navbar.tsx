fc14d876db2829fb3dac763dc2bff043c2574621
Rosc0e <blakekpck6@gmail.com>
2023-06-09 02:05:41 -0700

    logo stroke

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -5,7 +5,9 @@ function Navbar() {
     <div className=" 	">
       <div className="navbar w-5/6 border border-black mx-auto mt-10 transition ease-in-out delay-150 duration-300 ">
         <div className="flex-1">
-          <a className="btn btn-ghost normal-case text-xl">VALIS</a>
+          <a className="btn btn-ghost border">
+            <span className="text-outline text-3xl">VALIS</span>
+          </a>
         </div>
         <div className="flex-none">
           <ul className="menu menu-horizontal px-1">
@@ -14,7 +16,7 @@ function Navbar() {
             </li>
             <li>
               <details>
-                <summary>Menu</summary>
+                <summary className="text-1xl">Menu</summary>
                 <ul className="p-2 z-50 bg-base-100">
                   <li>
                     <a>Portfolio</a>

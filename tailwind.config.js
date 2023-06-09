55ed716d53dd938df0d8ad350bd7dc29b0a0aa38
Rosc0e <blakekpck6@gmail.com>
2023-06-08 19:33:25 -0700

    Initial COMMIT

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  safelist: [
+    '!duration-[0ms]',
+    '!delay-[0ms]',
+    'html.js :where([class*="taos:"]:not(.taos-init))'
+  ],
   content: [
+    { transform: (content) => content.replace(/taos:/g, ''), },
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+
+  plugins: [require("daisyui"), require('taos/plugin')],
+  daisyui: {
+    themes: ["lofi"],
   },
-  plugins: [],
 }

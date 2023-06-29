ce0ab76ad38b2e730b53e443f1c9d8110dc42bc7
Rosc0e <blakekpck6@gmail.com>
2023-06-29 09:17:24 -0700

    webm - > mp4

--- src/app/components/BackGround.tsx
+++ src/app/components/BackGround.tsx
@@ -14,7 +14,7 @@ function BackGround() {
           src="//starlink.ua/media/mod_starlink/car-blur.webm"
           type="video/webm"
         /> */}
-        <source src="/output.webm" type="video/webM" />
+        <source src="/rain.mp4" type="video/mp4" />
       </video>
     </div>
   );
